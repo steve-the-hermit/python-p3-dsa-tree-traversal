48ed73568a7adb20b679ef4a8b52efdd59beb12b
steve-the-hermit <stevemacharia309@gmail.com>
2023-09-13 18:12:29 +0300

    done

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,21 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, target_id):
+        if self.root is None:
+            return None
+        
+        # Helper function for depth-first traversal
+        def dfs_search(node):
+            if node.get('id') == target_id:
+                return node
+
+            for child in node.get('children', []):
+                result = dfs_search(child)
+                if result:
+                    return result
+
+            return None
+
+        return dfs_search(self.root)
